a2f7352edb6cf35359ad9e8b91506370e54c31db
Kemal Fadillah <fadillzzz@gmail.com>
2018-11-30 23:31:47 -0800

    Indexes email

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email: {type: String, required: true},
+    email: {type: String, required: true, index: true},
     password: {type: String, required: true}
 });
 

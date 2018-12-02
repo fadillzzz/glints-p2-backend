6599b89923389d7638724f7e489379351a4c0b97
Kemal Fadillah <fadillzzz@gmail.com>
2018-12-02 05:20:53 -0800

    Adds function to check user's existence

--- api/services/user.service.js
+++ api/services/user.service.js
@@ -12,6 +12,10 @@ class UserService {
         return user;
     }
 
+    async exists(id) {
+        return await User.countDocuments({_id: id}) > 0;
+    }
+
     async create(email, password) {
         const hashedPassword = await this.authService.generateHash(password);
         const user = await User.create({email, password: hashedPassword});
